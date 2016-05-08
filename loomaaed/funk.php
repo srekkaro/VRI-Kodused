59130be320c46cf01c2cc21de0fd5ad73f88e4a9
srekkaro <srekkaro@users.noreply.github.com>
2016-05-08 23:49:39 +0300

    Kodune töö number 12

---
+++ loomaaed/funk.php
@@ -0,0 +1,85 @@
+<?php
+
+
+function connect_db(){
+	global $connection;
+	$host="localhost";
+	$user="test";
+	$pass="t3st3r123";
+	$db="test";
+	$connection = mysqli_connect($host, $user, $pass, $db) or die("ei saa ühendust mootoriga- ".mysqli_error());
+	mysqli_query($connection, "SET CHARACTER SET UTF8") or die("Ei saanud baasi utf-8-sse - ".mysqli_error($connection));
+}
+
+function logi(){
+	// siia on vaja funktsionaalsust (13. nädalal)
+
+	include_once('views/login.html');
+}
+
+function logout(){
+	$_SESSION=array();
+	session_destroy();
+	header("Location: ?");
+}
+
+function kuva_puurid(){
+	global $connection;
+	$puurid=array();
+	$puuride_numbrid="SELECT DISTINCT puur FROM srekkaro_loomaaed";
+
+	$tulemus= mysqli_query($connection, $puuride_numbrid) or die ("Viga andmebaasis - ".mysqli_error($connection));
+	while ($rida = mysqli_fetch_assoc($tulemus)){
+		$mitmes_puur=$rida['puur'];
+		$loomade_p2ring="SELECT nimi FROM srekkaro_loomaaed WHERE puur=$mitmes_puur";
+		$tulemus2=mysqli_query($connection, $loomade_p2ring) or die ("Viga andmebaasis - ".mysqli_error($connection));
+		while ($rida2 = mysqli_fetch_assoc($tulemus2)){
+				$puurid[$rida['puur']][]=$rida2['nimi'];
+			}
+	}
+	ksort($puurid);
+	?><pre><?php
+	print_r($puurid);
+	?></pre><?php
+
+	include_once('views/puurid.html');
+	
+}
+
+function lisa(){
+	// siia on vaja funktsionaalsust (13. nädalal)
+	
+	include_once('views/loomavorm.html');
+	
+}
+
+function upload($name){
+	$allowedExts = array("jpg", "jpeg", "gif", "png");
+	$allowedTypes = array("image/gif", "image/jpeg", "image/png","image/pjpeg");
+	$extension = end(explode(".", $_FILES[$name]["name"]));
+
+	if ( in_array($_FILES[$name]["type"], $allowedTypes)
+		&& ($_FILES[$name]["size"] < 100000)
+		&& in_array($extension, $allowedExts)) {
+    // fail õiget tüüpi ja suurusega
+		if ($_FILES[$name]["error"] > 0) {
+			$_SESSION['notices'][]= "Return Code: " . $_FILES[$name]["error"];
+			return "";
+		} else {
+      // vigu ei ole
+			if (file_exists("pildid/" . $_FILES[$name]["name"])) {
+        // fail olemas ära uuesti lae, tagasta failinimi
+				$_SESSION['notices'][]= $_FILES[$name]["name"] . " juba eksisteerib. ";
+				return "pildid/" .$_FILES[$name]["name"];
+			} else {
+        // kõik ok, aseta pilt
+				move_uploaded_file($_FILES[$name]["tmp_name"], "pildid/" . $_FILES[$name]["name"]);
+				return "pildid/" .$_FILES[$name]["name"];
+			}
+		}
+	} else {
+		return "";
+	}
+}
+
+?>
